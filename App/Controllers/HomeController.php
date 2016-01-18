8a0aeb60d8d78fb41437508e9c786e0178471913
Shaoting Wu <alice.wu.nz@gmail.com>
2016-01-18 16:56:43 +1300

    nothing change

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -19,6 +19,17 @@ class HomeController
 		$view->render();
 	}
  
-	 
+	 function search() 
+	{
+		if(! isset($_GET['q'])){
+			$q = "";
+		} else {
+			$q = $_GET['q'];
+		}
+		$restaurants = RestaurantModel::search($q);
+
+		$view = new SearchResultsView(compact('restaurants', 'q'));
+		$view->render();
+	}
 
 }
