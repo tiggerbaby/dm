8de61ebd0ea2464e26659cd6a557b04d09fb2f68
Alice <alice.wu.nz@gmail.com>
2016-03-23 10:35:02 +1300

    Changing booking success page.

--- public_html/templates/bookingsuccess.inc.php
+++ public_html/templates/bookingsuccess.inc.php
@@ -1,7 +1,8 @@
-<div class="jumbotron backgroundColorGreen">
+<!-- <div class="jumbotron backgroundColorGreen"> -->
 <div class="container">
-		<div class="col-xs-12 text-center">
-			<h2>Booking successfully!</h2>
-		</div>
+	<h3 class="alert alert-success" role="alert"><span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span> Thanks for making a reservation from Dning Mate!</h3>
+		<p>Here are your booking details. If you have any question, please do not hesitate to contact us! </p>
+
+
 	</div>
 </div>
